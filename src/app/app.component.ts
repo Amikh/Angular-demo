d7b1ce4519e4aad05e69d24c99bc70f12b97d7ee
AM <amikh05@gmail.com>
2021-07-30 17:55:54 +0300

    add function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpHeaders } from '@angular/common/http';
+
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+
+export class Product {
+  constructor(
+    public id: number,
+    public name: string,
+    public price: number
+    ) {
+   }
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+@Injectable()
 export class AppComponent {
   title = 'sample-angular-app';
-}
+
+   private REST_API_SERVER = "http://localhost:8080/product";
+
+   constructor(private http: HttpClient) { }
+   httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }  
+
+
+  
+  // isGetProduct() : boolean{
+  //     this.http.get(this.REST_API_SERVER).subscribe(data => {
+  //     console.log(data);
+  //   },
+  //     error => {
+  //         console.log('Log the error here: ', error);
+  //     });
+  //   return false;
+  // }
+  
+
+    isGetProduct() : boolean{
+      this.http.get(this.REST_API_SERVER).subscribe(data => {
+      console.log(data);
+
+
+    },
+      error => {
+          console.log('Log the error here: ', error);
+      });
+
+
+
+
+    return false;
+  }
+
+    
+  
+
+
+  }
+
+
+
